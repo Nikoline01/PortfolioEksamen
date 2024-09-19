9bf438a920e18e1321045c1ad8bdcf3f07cfea97
Nikoline01 <146435826+Nikoline01@users.noreply.github.com>
2024-09-19 19:15:26 +0200

    send

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter
-      basename={
-        import.meta.env.DEV ? "/" : "/react-vite-rest-post-app-with-auth/"
-      }
-    >
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
