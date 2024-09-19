978611a322771406d8e49aa4b23384021cf3fb31
Nikoline01 <146435826+Nikoline01@users.noreply.github.com>
2024-09-19 19:10:08 +0200

    send

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./App.css";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter
+      basename={
+        import.meta.env.DEV ? "/" : "/react-vite-rest-post-app-with-auth/"
+      }
+    >
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
